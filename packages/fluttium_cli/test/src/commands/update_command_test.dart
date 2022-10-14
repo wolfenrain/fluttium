2291b40d1c88c942e36c6521a0600c6922e93f97
Jochum van der Ploeg <jochum.vanderploeg@verygood.ventures>
2022-10-14 13:09:35 +0200

    fix: using wrong `packageVersion` for the update command (#17)
    * fix: using wrong `packageVersion` for the update command
    
    * fix: using wrong `packageVersion` for the update command

--- packages/fluttium_cli/test/src/commands/update_command_test.dart
+++ packages/fluttium_cli/test/src/commands/update_command_test.dart
@@ -2,7 +2,8 @@ import 'dart:io';
 
 import 'package:fluttium_cli/src/command_runner.dart';
 import 'package:fluttium_cli/src/commands/commands.dart';
-import 'package:mason/mason.dart';
+import 'package:fluttium_cli/src/version.dart';
+import 'package:mason/mason.dart' hide packageVersion;
 import 'package:mocktail/mocktail.dart';
 import 'package:pub_updater/pub_updater.dart';
 import 'package:test/test.dart';
