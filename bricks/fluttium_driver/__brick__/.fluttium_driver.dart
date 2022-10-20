e9a8675b5bd84010a4f7d117da7a922bd3d1eea5
Jochum van der Ploeg <jochum.vanderploeg@verygood.ventures>
2022-10-20 15:01:28 +0200

    feat: wait for finding results

--- bricks/fluttium_driver/__brick__/.fluttium_driver.dart
+++ bricks/fluttium_driver/__brick__/.fluttium_driver.dart
@@ -1,8 +1,7 @@
 import 'dart:async';
 import 'dart:ui';
 
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
@@ -29,47 +28,10 @@ void main() {
 }
 
 extension on WidgetTester {
-  Finder _findByText(String text) {
-    final regexp = RegExp('^$text\$');
-    var finder = find.bySemanticsLabel(regexp);
-    if (finder.evaluate().isEmpty) {
-      finder = find.byTooltip(text);
-      if (finder.evaluate().isEmpty) {
-        finder = find.textContaining(regexp);
-      }
-    }
-    return finder;
-  }
-
-  Future<void> expectVisible(String text) async {
-    await manager.start();
-    if (_findByText(text).evaluate().isNotEmpty) {
-      await manager.done();
-    } else {
-      await manager.fail();
-    }
-  }
-
-  Future<void> expectNotVisible(String text) async {
-    await manager.start();
-    if (_findByText(text).evaluate().isEmpty) {
-      await manager.done();
-    } else {
-      await manager.fail();
-    }
-  }
-
-  Future<void> tapOn(String text) async {
-    await manager.start();
-    await tap(_findByText(text));
-    await pumpAndSettle();
-    await manager.done();
-  }
-
   Future<void> takeScreenshot(String name) async {
     await manager.start();
-    final RenderRepaintBoundary boundary =
-        firstRenderObject(find.byType(RepaintBoundary));
+    final boundary = firstRenderObject(find.byType(RepaintBoundary))
+        as RenderRepaintBoundary;
     final image = await boundary.toImage();
     final byteData = await image.toByteData(format: ImageByteFormat.png);
     final pngBytes = byteData!.buffer.asUint8List();
@@ -77,15 +39,124 @@ extension on WidgetTester {
     await manager.done();
   }
 
+  Future<void> expectVisible(String text) async {
+    await manager.start();
+    try {
+      final finder = await _findOrWait(text);
+      if (finder.evaluate().isEmpty) {
+        throw Exception('Could not find $text');
+      }
+      await manager.done();
+    } catch (err) {
+      await manager.fail();
+    }
+  }
+
+  Future<void> expectNotVisible(String text) async {
+    await manager.start();
+    try {
+      final finder = await _findOrWait(text);
+      if (finder.evaluate().isNotEmpty) {
+        throw Exception('Found $text');
+      }
+      await manager.done();
+    } catch (err) {
+      await manager.fail();
+    }
+  }
+
+  Future<void> tapOn(String text) async {
+    await manager.start();
+    try {
+      final finder = await _findOrWait(text);
+      if (finder.evaluate().isEmpty) {
+        throw Exception('Could not find $text');
+      }
+      await tap(await _findOrWait(text));
+      await pumpAndSettle();
+      await manager.done();
+    } catch (err) {
+      await manager.fail();
+    }
+  }
+
   Future<void> inputText(String text) async {
     await manager.start();
-    var chars = [];
+    final chars = <String>[];
     for (final char in text.split('')) {
       chars.add(char);
       testTextInput.enterText(chars.join());
       await pumpAndSettle();
     }
     await manager.done();
+  }
+
+  Future<Finder> _findOrWait(
+    String text, {
+    Duration timeout = const Duration(seconds: 10),
+  }) async {
+    final regexp = RegExp('^$text\$');
+    final finder = find.bySemanticOrText(regexp);
+
+    // "Borrowed" from Patrol:
+    // https://github.com/leancodepl/patrol/blob/56cdba8a9abbfa76fe5603c82fbbb2143cf8e79c/packages/patrol/lib/src/custom_finders/patrol_tester.dart#L276
+    final end = binding.clock.now().add(timeout);
+    while (finder.hitTestable().evaluate().isEmpty) {
+      final now = binding.clock.now();
+      if (now.isAfter(end)) {
+        throw Exception('Timeout waiting for $text');
+      }
+
+      await pump(const Duration(milliseconds: 100));
+    }
+    return finder;
+  }
+}
+
+extension on CommonFinders {
+  Finder bySemanticOrText(RegExp pattern, {bool skipOffstage = true}) {
+    if (WidgetsBinding.instance.pipelineOwner.semanticsOwner == null) {
+      throw StateError(
+        'Semantics are not enabled. '
+        'Make sure to call tester.ensureSemantics() before using '
+        'this finder, and call dispose on its return value after.',
+      );
+    }
+
+    return byElementPredicate(
+      (Element element) {
+        // Multiple elements can have the same renderObject, we want the "owner"
+        // of the renderObject, i.e. the RenderObjectElement.
+        if (element is! RenderObjectElement) return false;
+
+        var text = element.renderObject.debugSemantics?.label;
+        if (text == null) {
+          final widget = element.widget;
+          if (widget is Tooltip) {
+            text = widget.message;
+          } else if (widget is EditableText) {
+            text = widget.controller.text;
+          } else if (widget is Text) {
+            if (widget.data != null) {
+              text = widget.data;
+            } else {
+              assert(
+                widget.textSpan != null,
+                'Text widget must have data or textSpan',
+              );
+              text = widget.textSpan!.toPlainText();
+            }
+          } else if (widget is RichText) {
+            text = widget.text.toPlainText();
+          }
+
+          if (text == null) return false;
+        }
+
+        return pattern.hasMatch(text);
+      },
+      skipOffstage: skipOffstage,
+    );
   }
 }
 
@@ -98,19 +169,22 @@ class FluttiumManager {
 
   Future<void> fail() async {
     await send('fail');
-    _hasFailed = true; 
+    _hasFailed = true;
   }
 
   Future<void> send(String action, [Object? data]) async {
     if (_hasFailed) {
-      return Completer().future;
+      return Completer<void>().future;
     }
     final message = 'fluttium:$action:$data;';
 
-    final maxLength = 800;
-    for (int i = 0; i < message.length; i += maxLength) {
-      int offset = i + maxLength;
-      final piece = message.substring(i, offset >= message.length ? message.length : offset);
+    const maxLength = 800;
+    for (var i = 0; i < message.length; i += maxLength) {
+      final offset = i + maxLength;
+      final piece = message.substring(
+        i,
+        offset >= message.length ? message.length : offset,
+      );
       debugPrint(piece);
     }
   }
