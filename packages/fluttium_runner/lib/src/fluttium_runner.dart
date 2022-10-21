53bbf2fa8ebbc7c6330f960a5b1df8d468b04703
Jochum van der Ploeg <jochum@vdploeg.net>
2022-10-21 21:35:11 +0200

    feat: running multiple flows at once (#74)

--- packages/fluttium_runner/lib/src/fluttium_runner.dart
+++ packages/fluttium_runner/lib/src/fluttium_runner.dart
@@ -26,7 +26,6 @@ class FluttiumRunner {
     Logger? logger,
     ProcessManager? processManager,
   })  : _logger = logger ?? Logger(),
-        _driver = File(join(projectDirectory.path, '.fluttium_driver.dart')),
         _processManager = processManager ?? const LocalProcessManager();
 
   /// The flow file to run.
@@ -63,7 +62,7 @@ class FluttiumRunner {
   /// success status of the step that matches the index in the list.
   final List<bool?> _stepStates = [];
 
-  final File _driver;
+  late File _driver;
 
   final Map<String, dynamic> _vars = {};
 
@@ -181,10 +180,11 @@ class FluttiumRunner {
     _convertFlowToVars();
 
     _generator ??= await MasonGenerator.fromBundle(fluttiumDriverBundle);
-    await _generator!.generate(
+    final result = await _generator!.generate(
       DirectoryGeneratorTarget(projectDirectory),
       vars: _vars,
     );
+    _driver = File(result.first.path);
   }
 
   Future<void> _cleanupProject() async {
@@ -227,7 +227,7 @@ class FluttiumRunner {
       [
         'flutter',
         'run',
-        '.fluttium_driver.dart',
+        _driver.path,
         '-d',
         deviceId,
         if (flavor != null) ...['--flavor', flavor!],
