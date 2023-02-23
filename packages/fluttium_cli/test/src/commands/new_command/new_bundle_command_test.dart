7efeca83e0d72196be2cc86b5008b0dfcf2ffbb0
Jochum van der Ploeg <jochum.vanderploeg@verygood.ventures>
2023-02-23 16:39:53 +0100

    chore(fluttium_cli): 0.1.0-dev.16

--- packages/fluttium_cli/test/src/commands/new_command/new_bundle_command_test.dart
+++ packages/fluttium_cli/test/src/commands/new_command/new_bundle_command_test.dart
@@ -1,3 +1,4 @@
+import 'package:args/args.dart';
 import 'package:fluttium_cli/src/commands/new_command/new_bundle_command.dart';
 import 'package:mason/mason.dart' hide packageVersion;
 import 'package:mocktail/mocktail.dart';
@@ -7,10 +8,22 @@ class _MockLogger extends Mock implements Logger {}
 
 class _MockMasonGenerator extends Mock implements MasonGenerator {}
 
+class _MockGeneratorHooks extends Mock implements GeneratorHooks {}
+
 class _MockMasonBundle extends Mock implements MasonBundle {}
+
+class _MockArgResults extends Mock implements ArgResults {}
+
+class _MockProgress extends Mock implements Progress {}
+
+class _FakeGeneratorTarget extends Fake implements GeneratorTarget {}
 
 void main() {
   group('NewBundleCommand', () {
+    setUp(() {
+      registerFallbackValue(_FakeGeneratorTarget());
+    });
+
     test('correct invocation', () {
       final bundle = _MockMasonBundle();
       when(() => bundle.name).thenReturn('fluttium_mock');
@@ -21,6 +34,63 @@ void main() {
         generatorFromBundle: (_) async => _MockMasonGenerator(),
         generatorFromBrick: (_) async => _MockMasonGenerator(),
         bundle: bundle,
+      );
+
+      expect(
+        command.invocation,
+        equals('fluttium new mock <name> [arguments]'),
+      );
+    });
+
+    test('uses built-in bundle when retrieving fails', () async {
+      final logger = _MockLogger();
+      final generateProgress = _MockProgress();
+      when(() => logger.progress(any())).thenReturn(generateProgress);
+
+      final argsResult = _MockArgResults();
+      when(() => argsResult.rest).thenReturn(['name']);
+
+      final bundle = _MockMasonBundle();
+      when(() => bundle.name).thenReturn('fluttium_mock');
+      when(() => bundle.vars).thenReturn({});
+      when(() => bundle.version).thenReturn('0.1.0');
+
+      final generator = _MockMasonGenerator();
+      final hooks = _MockGeneratorHooks();
+      when(
+        () => hooks.preGen(
+          vars: any(named: 'vars'),
+          onVarsChanged: any(named: 'onVarsChanged'),
+        ),
+      ).thenAnswer((_) async {});
+      when(() => generator.hooks).thenReturn(hooks);
+      when(
+        () => generator.generate(
+          any(),
+          vars: any(named: 'vars'),
+          logger: any(named: 'logger'),
+        ),
+      ).thenAnswer((_) async => []);
+
+      final command = NewBundleCommand(
+        logger: logger,
+        generatorFromBundle: (_) async => generator,
+        generatorFromBrick: (_) async => throw Exception(),
+        bundle: bundle,
+      )..argResultOverrides = argsResult;
+
+      await command.run();
+
+      verify(
+        () => logger.detail(
+          any(that: contains('Building generator from brick failed:')),
+        ),
+      );
+
+      verify(
+        () => logger.detail(
+          any(that: contains('Building generator from bundle')),
+        ),
       );
 
       expect(
