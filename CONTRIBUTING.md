5c910bb63d959f17d9d1b0fe7dac4e171dca205f
Jochum van der Ploeg <jochum.vanderploeg@verygood.ventures>
2023-03-01 20:54:33 +0100

    ci: add e2e tests for user flows

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -67,7 +67,8 @@ Fluttium requires a bit of setup to develop locally, thankfully we have simple s
 does all the heavy lifting for you:
 
 ```shell
-dart ./tools/setup_local_environment.dart
+cd tools && dart pub get && cd ..
+dart tools/setup_local_environment.dart
 ```
 
 After that you can install the local version of the CLI to test your changes:
--- tools/setup_local_environment.dart
+++ tools/setup_local_environment.dart
@@ -1,14 +1,15 @@
 import 'dart:io';
+import 'package:path/path.dart' as path;
 
 void main() {
   final localSetup = '''
 dependency_overrides:
   fluttium:
-    path: ${Directory.current.uri.resolve('packages/fluttium').path}
+    path: ${path.canonicalize(Directory.current.uri.resolve('packages/fluttium').path)}
   fluttium_protocol:
-    path: ${Directory.current.uri.resolve('packages/fluttium_protocol').path}
+    path: ${path.canonicalize(Directory.current.uri.resolve('packages/fluttium_protocol').path)}
   fluttium_interfaces:
-    path: ${Directory.current.uri.resolve('packages/fluttium_interfaces').path}
+    path: ${path.canonicalize(Directory.current.uri.resolve('packages/fluttium_interfaces').path)}
 ''';
 
   File('example/pubspec_overrides.yaml').writeAsStringSync(localSetup);
